7a971ade8aef7c94942bd8d59b04bdbe096ca747
Bo van Breukelen <b@vbreukelen.com>
2019-01-04 18:16:12 +0100

    Updated colofon

--- js/colofon.js
+++ js/colofon.js
@@ -1,41 +1,39 @@
-window.colofon= `
+window.colofon = `
 <section class="sheet padding-7mm">
-    Teams met contactgegevens<br>
-    <div class="coverTeams">
-        <div class="coverTeam">
-            <span>Bedrijfsleiding</span>    
-            <span>beheerder@leussinkbad.nl</span>
-        </div>
-        <div class="coverTeam">
-            <span>Bestuur</span>    
-            <span>bestuur@leussinkbad.nl</span>
-        </div>
-        <div class="coverTeam">
-            <span>Toezichthouders</span>    
-            <span>toezichthouders@leussinkbad.nl</span>
-        </div>
-        <div class="coverTeam">
-            <span>Kiosk</span>    
-            <span>kiosk@leussinkbad.nl</span>
-        </div>
-        <div class="coverTeam">
-            <span>Groenvoorziening</span>    
-            <span>groenvoorziening@leussinkbad.nl</span>
-        </div>
-        <div class="coverTeam">
-            <span>Promoteam</span>    
-            <span>promoteam@leussinkbad.nl</span>
-        </div>
-        <div class="coverTeam">
-            <span>Activiteitencommissie</span>    
-            <span>activiteiten@leussinkbad.nl</span>
-        </div>
-        <div class="coverTeam">
-            <span>Techniek</span>    
-            <span>techniek@leussinkbad.nl</span>
-        </div>    
-    </div>
+    <br/><br/><br/><br/>
+    <div style="text-align: center">
+        <i>versie: 4 januari 2019</i><br/>
+   </div>
     
-    Wie te benaderen voor wijzigingen/fouten
-    Hoe nieuwe foto aan te leveren etc.
+    <br/><br/>
+    
+    <h3 style="margin-bottom: 3px;">Gegevens zwembad</h3>
+    Stichting Zwembad Laren<br/>
+    Holterweg 45<br/>
+    7245 SB Laren GLD<br/>
+    T: 0573 401748<br/>
+    E: info@leussinkbad.nl<br/>
+    W: leussinkbad.nl<br/>
+    KvK: 41038846<br/>
+    
+    <br/><br/>
+    
+    <h3 style="margin-bottom: 3px;">Contactgegevens Teams</h3>
+    <table border="0">
+      <tr><td>Bedrijfsleiding:</td><td>beheerder@leussinkbad.nl</td></tr>
+      <tr><td>Bestuur:</td><td>bestuur@leussinkbad.nl</td></tr>
+      <tr><td>Toezichthouders:</td><td>toezichthouders@leussinkbad.nl</td></tr>
+      <tr><td>Kiosk:</td><span class="Apple-tab-span">	</span><td>kiosk@leussinkbad.nl</td></tr>
+      <tr><td>Promoteam:</td><td>promoteam@leussinkbad.nl</td></tr>
+      <tr><td>Activiteitencommissie:</td><td>activiteiten@leussinkbad.nl</td></tr>
+      <tr><td>Groenvoorziening:</td><td>groenvoorziening@leussinkbad.nl</td></tr>
+      <tr><td>Techniek:</td><td>techniek@leussinkbad.nl</td></tr>
+      <tr><td style="min-width:250px"></td><td></td></tr>    
+    </table>
+    
+    <br/><br/><br/>
+      
+    <h3 style="margin-bottom: 3px;">Wijzigingen</h3>
+    Geef wijzigingen en nieuwe pasfoto's aub door op bestuur@leussinkbad.nl.<br/>
+    Als er wijzigingen zijn zal er eens per kwartaal een versie van dit smoelenboek uitgegeven worden.<br/>
 </section>`;
